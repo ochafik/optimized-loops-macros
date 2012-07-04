aaad8135104a71c01ee181c9491598cbe605e718
Olivier Chafik <olivier.chafik@gmail.com>
2012-07-04 23:59:28 +0100

    nit

--- src/main/scala/scala/inlinable/InlinableRangeMacros.scala
+++ src/main/scala/scala/inlinable/InlinableRangeMacros.scala
@@ -6,7 +6,7 @@ import reflect.makro.Context
 private[inlinable] object InlinableRangeMacros
 {
   private[this] val errorMessageFormat = "Failed to optimize loop (%s)"
-  private[this] val successMessage = "Rewrote this for-comprehension into a while loop"
+  private[this] val successMessage = "Rewrote this for-comprehension as a while loop"
   
   def rangeForeachImpl(c: Context)(f: c.Expr[Int => Unit]): c.Expr[Unit] = {
     c.Expr[Unit](
