a1488a6e1c7268b653adf55d9964335d8d7c1499
Olivier Chafik <olivier.chafik@gmail.com>
2012-07-04 02:18:16 +0100

    Initial commit (copied from Scalaxy repo at InlinableRange)

---
+++ src/main/scala/scala/inlinable/CommonMatchers.scala
@@ -0,0 +1,26 @@
+package scala.inlinable
+
+import language.experimental.macros
+import reflect.makro.Context
+
+trait CommonMatchers
+{
+  val universe: reflect.makro.Universe
+  import universe._
+	
+  object IntConstant {
+    def unapply(tree: Tree) = Option(tree) collect {
+      case Literal(Constant(v: Int)) => v
+    }
+  }
+  object PositiveIntConstant {
+    def unapply(tree: Tree) = Option(tree) collect {
+      case Literal(Constant(v: Int)) if v > 0 => v
+    }
+  }
+  object NegativeIntConstant {
+    def unapply(tree: Tree) = Option(tree) collect {
+      case Literal(Constant(v: Int)) if v < 0 => v
+    }
+  }
+}
